7dc9d32c85a3daa66aa8904dbf9171a428b266fd
Yanina Petrova <yaninapetrova63@gmail.com>
2023-05-10 11:15:56 +0200

    works

--- main.tf
+++ main.tf
@@ -1,13 +1,28 @@
-resource "azurerm_resource_group" "resource-group" {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = "true"
+  features {}
+}
+
+resource "azurerm_resource_group" "aks-test" {
   name     = var.resource_group
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "aks-test" {
-  name                = "aks-test"
-  resource_group_name = var.resource_group
+resource "azurerm_kubernetes_cluster" "aks-test-cluster" {
+  name                = "aks-test-cluster"
+  resource_group_name = azurerm_resource_group.aks-test.name
   location            = var.location
-  dns_prefix          = "exampleaks1"
+
+  dns_prefix          = "exampleaks"
 
   default_node_pool {
     name       = "default"
